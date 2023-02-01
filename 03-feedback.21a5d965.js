603b8d225d12f8bef35f02108ab2dda484be4692
DmytroRudenko11 <117588754+DmytroRudenko11@users.noreply.github.com>
2023-02-01 18:36:31 +0000

    Deploying to gh-pages from @ DmytroRudenko11/goit-js-hw-08@a4e07815f23f5f8b69560ef125f4ab0e2165db62 🚀

--- 03-feedback.21a5d965.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input"),document.querySelector("textarea");document.querySelector("form").addEventListener("input",(function({target:e}){localStorage.setItem("feedback-form-state",JSON.stringify(e.value)),fillField()}));
-//# sourceMappingURL=03-feedback.21a5d965.js.map
--- 03-feedback.77ce90e8.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("input"),document.querySelector("textarea");document.querySelector("form").addEventListener("input",(function(e){var t=e.target;localStorage.setItem("feedback-form-state",JSON.stringify(t.value)),fillField()}))}();
-//# sourceMappingURL=03-feedback.77ce90e8.js.map
